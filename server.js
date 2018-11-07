227870f082e8d019930b3ab36df862c168e7865e
Chris Klanac <cklanac@users.noreply.github.com>
2018-11-07 08:21:57 -0500

    feat: add client to demo

---
+++ server.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const express = require('express');
+const morgan = require('morgan');
+
+const { PORT } = require('./config');
+
+// Create an Express application
+const app = express();
+
+// Log all requests
+app.use(morgan('common'));
+
+// Create a static webserver
+app.use(express.static('public'));
+
+// Catch-all 404
+app.use(function (req, res, next) {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// Catch-all Error handler
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: app.get('env') === 'development' ? err : {}
+  });
+});
+
+// Listen for incoming connections
+app.listen(PORT, function () {
+  console.info(`Server listening on ${this.address().port}`);
+}).on('error', err => {
+  console.error(err);
+});
